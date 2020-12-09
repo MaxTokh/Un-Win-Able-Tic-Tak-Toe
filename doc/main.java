58f844652b0342660ef5af0ce29af3b7673b7647
MaxTokh <61331302+MaxTokh@users.noreply.github.com>
2020-12-09 11:32:54 -0700

    Add files via upload

---
+++ doc/main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+//  Un-Win-Able Tic-Tak-Toe
+
+public class Main {
+  double[][][] board = new double[3][3][3]{
+    double[][][] board1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+
+//Print | between everything
+    for (int i = 0; i < board.length; i++) {
+      if (i > 0) {
+        System.out.print(" | ");
+      }
+      System.out.print(values[i]);
+    }
+
+
+//Whem computer moves first
+
+  //Play from middle
+    cTake [1][1];
+
+    //Play from corner
+
+
+//When player moves firts
+    double[][][] board = new double[1][][];
+    for (i = 0; i < inputs.length; i++)
+    {
+       inputs[i] = in.nextDouble();
+    }
+
+
+
+
+
+  }
+}
